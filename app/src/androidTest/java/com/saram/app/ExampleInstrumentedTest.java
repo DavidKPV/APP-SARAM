02a289802e8ed0ed137f6c610c308d4e3a20e788
DavidKPV <daviddago10@gmail.com>
2020-06-08 09:11:08 -0500

    SE REALIZARON MÁS CAMBIOS DE NOMBRE

--- app/src/androidTest/java/com/saram/app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/saram/app/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.ivonne.saram", appContext.getPackageName());
+        assertEquals("com.ivonne.com.saram.app", appContext.getPackageName());
     }
 }
