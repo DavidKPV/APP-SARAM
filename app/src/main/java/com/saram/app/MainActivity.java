9fe3b3df676e6d3de08dfc607820dafa8ea98567
DavidKPV <daviddago10@gmail.com>
2020-06-23 13:56:57 -0500

    Se agregó funcionalidad al Cerrar Sesión
    Se aseguró de que fuera funcional el cerrar sesión, se desarrolló un fragmento nuevo para dar funcionalidad así como también limitar la flecha back que tienen por default los dispositivos móviles, así mismo se dejó preparado el SharedPreferences con el que se trabajará.

--- app/src/main/java/com/saram/app/MainActivity.java
+++ app/src/main/java/com/saram/app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.saram.app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +16,22 @@ public class MainActivity extends AppCompatActivity {
     TextView tvOlvido, tvRegistro;
     Button btnIngreso;
 
+    // ESTE MÉTODO EVITA QUE SE REGRESE CON LA FLECHA DE RETORNO QUE TODOS TENEMOS
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // VERIFICA SI EXISTE O NO UNA SESION INICIADA
+        verifica();
+
+        // SE ACTIVA EL MÉTODO PARA CREAR EL SHARED PREFERENCES
+        CrearSharedPreferences();
 
         // SE ENLANZAN LOS CONTROLADORES CON LA VISTA
         tvOlvido = (TextView) findViewById(R.id.tvOlvido);
@@ -51,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
     // MÉTODO DE INGRESO AL INICIO DE LA APP
     private void ingresar(){
-        // AÚN NO SE PROGRAMA NADA, SIN EMBARGO ESTO ES PARA PROBAR EL INICIO DE LA APP
+        // AÚN NO SE PROGRAMA NADA CON VOLLEY, SIN EMBARGO ESTO ES PARA PROBAR EL INICIO DE LA APP
         Intent intent_inicio = new Intent(this, inicioActivity.class);
         startActivity(intent_inicio);
     }
@@ -68,5 +82,37 @@ public class MainActivity extends AppCompatActivity {
         Intent intentregistro = new Intent(this, registroActivity.class);
         // SE INICIA LA NUEVA ACTIVITY
         startActivity(intentregistro);
+    }
+
+    // SE CREA LA FUNCIÓN DEL SHARED PREFERENCES
+    private void CrearSharedPreferences(){
+        // SE INSTANCIA EL SHARED                   Nombre del archivo,   forma en como se abrirá
+        SharedPreferences prefer1 = getSharedPreferences("MisDatos", Context.MODE_PRIVATE);
+
+        // SE ACTIVA LA EDICIÓN DEL ARCHIVO
+        SharedPreferences.Editor editor = prefer1.edit();
+
+        // SE MANDAN LOS VALORES QUE QUEREMOS ALMACENAR
+        // Nombre de la variable, valor de la variable
+        editor.putBoolean("sesion", false);
+
+        // SE MANDA UNA INSTRUCCIÓN COMMIT PARA QUE SE GUARDEN LOS CAMBIOS
+        editor.commit();
+    }
+
+    private void verifica(){
+        // ACTIVAMOS EL EDITOR DEL SHARED
+        SharedPreferences sp1 = getSharedPreferences("MisDatos", Context.MODE_PRIVATE);
+
+        // TRAEMOS EL VALOR DE LA SESION
+        boolean val = sp1.getBoolean("sesion", false);
+
+        // SE INSTANCIA EL ACTIVITY DEL INICIO DE LA APP    contexto, nombre de la clase compilada
+        Intent intent1 = new Intent(this, inicioActivity.class);
+
+        if(val == true) {
+            // SE MANDA A EJECUTAR EL ACTIVITY DE INICIO DE LA APP
+            startActivity(intent1);
+        }
     }
 }
--- app/src/main/java/com/saram/app/inicioActivity.java
+++ app/src/main/java/com/saram/app/inicioActivity.java
@@ -1,7 +1,11 @@
 package com.saram.app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
@@ -9,6 +13,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
@@ -23,12 +28,23 @@ public class inicioActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+    // ESTE MÉTODO EVITA QUE SE REGRESE CON LA FLECHA DE RETORNO QUE TODOS TENEMOS
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // VERIFICA QUE HAYA UNA SESION ACTIVA
+        activa_inicio();
+        verifica();
+
         /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,16 +55,20 @@ public class inicioActivity extends AppCompatActivity {
         });*/
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+
         // ESTE ES EL QUE CAMBIA EL COLOR DE TEXTO DEL ITEM
         navigationView.setItemTextColor(ColorStateList.valueOf(getResources().getColor(R.color.blanco)));
+
         //PARA CAMBIAR EL COLOR DEL ITEM DEL MENÚ
         navigationView.setItemIconTintList(ColorStateList.valueOf(getResources().getColor(R.color.blanco)));
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        // Pasando cada ID del menú como una posición de Ids porque cada
+        // menú debe ser considerado como destinos de nivel top.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_contactos, R.id.nav_info, R.id.nav_pprivacidad, R.id.nav_ubicacion, R.id.nav_home)
+                R.id.nav_contactos, R.id.nav_info, R.id.nav_pprivacidad, R.id.nav_ubicacion, R.id.nav_home, R.id.nav_cerrarS)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
@@ -58,6 +78,7 @@ public class inicioActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.inicio, menu);
+
         return true;
     }
 
@@ -67,5 +88,30 @@ public class inicioActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    private void activa_inicio(){
+        SharedPreferences sp1 = getSharedPreferences("MisDatos", Context.MODE_PRIVATE);
+        // SE MODIFICA EL VALOR DEL SHARED INICIALIZANDO EL EDITOR DEL SHARED
+        SharedPreferences.Editor editor = sp1.edit();
+        editor.putBoolean("sesion", true);
+        editor.commit();
+    }
+
+    private void verifica(){
+        // ACTIVAMOS EL EDITOR DEL SHARED
+        SharedPreferences sp1 = getSharedPreferences("MisDatos", Context.MODE_PRIVATE);
+
+        // TRAEMOS EL VALOR DE LA SESION
+        boolean val = sp1.getBoolean("sesion", false);
+
+        // SE INSTANCIA EL ACTIVITY DEL LOGIN DE LA APP    contexto, nombre de la clase compilada
+        Intent intent1 = new Intent(this, MainActivity.class);
+
+        if(val == false) {
+            // SE MANDA A EJECUTAR EL ACTIVITY DE INICIO DE LA APP
+            startActivity(intent1);
+        }
+    }
+
 
 }
