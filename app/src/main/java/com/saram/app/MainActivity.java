5b1e72c0b57961c6338a3e42a65ba40f28bf2290
DavidKPV <daviddago10@gmail.com>
2020-06-25 16:09:19 -0500

    Se programó el Back-End del web service (login y registro)
    No se han realizado pruebas aún, pero se tiene la esperanza de que funcione xD

--- app/src/main/java/com/saram/app/MainActivity.java
+++ app/src/main/java/com/saram/app/MainActivity.java
@@ -1,20 +1,49 @@
 package com.saram.app;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     // SE DECLARAN LOS OBJETOS UTILIZADOS
     TextView tvOlvido, tvRegistro;
     Button btnIngreso;
+    EditText etNombre, etPass;
+    TextInputLayout tilNombre, tilPass;
+    // SE DECLARAN LOS OBJETOS NECESARIOS PARA LA LIBRERÍA DE VOLLEY
+    RequestQueue requestQueue;
+    ProgressDialog progressDialog;
+
+    // CREAMOS UNA CADENA LA CUAL CONTENDRÁ LA CADENA DE NUESTRO WEB SERVICE
+    String HttpUri = "http://192.168.43.200:8080/SARAM-WEB/SARAM/public/api-login";
 
     // ESTE MÉTODO EVITA QUE SE REGRESE CON LA FLECHA DE RETORNO QUE TODOS TENEMOS
     @Override
@@ -37,6 +66,53 @@ public class MainActivity extends AppCompatActivity {
         tvOlvido = (TextView) findViewById(R.id.tvOlvido);
         tvRegistro = (TextView) findViewById(R.id.tvRegistro);
         btnIngreso = (Button) findViewById(R.id.btnIngreso);
+        etNombre = (EditText) findViewById(R.id.etNombre);
+        etPass = (EditText) findViewById(R.id.etPass);
+        tilNombre = (TextInputLayout) findViewById(R.id.tilNombre);
+        tilPass = (TextInputLayout) findViewById(R.id.tilPass);
+
+        // SE ACTIVAN LOS OBJETOS DE REQUEST QUEUE Y PROGRESS DIALOG
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        progressDialog = new ProgressDialog(MainActivity.this);
+
+        // SE CREAN LAS VALIDACIONES EN TIEMPO REAL
+        // VALIDACIÓN DENTRO DEL CAMPO NOMBRE
+        etNombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // PARA LIMPIAR LOS ERRORES DENTRO DEL PROCESO DE LLENADO DEL CAMPO
+                tilNombre.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        // VALIDACION DENTRO DEL CAMPO CONTRASEÑA
+        etPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // PARA LIMPIAR LOS ERRORES DENTRO DEL PROCESO DE LLENADO DEL CAMPO
+                tilNombre.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         // OYENTE DEL TEXTO DE RECUPERAR LA CONTRASEÑA
         tvOlvido.setOnClickListener(new View.OnClickListener() {
@@ -65,9 +141,81 @@ public class MainActivity extends AppCompatActivity {
 
     // MÉTODO DE INGRESO AL INICIO DE LA APP
     private void ingresar(){
-        // AÚN NO SE PROGRAMA NADA CON VOLLEY, SIN EMBARGO ESTO ES PARA PROBAR EL INICIO DE LA APP
-        Intent intent_inicio = new Intent(this, inicioActivity.class);
-        startActivity(intent_inicio);
+        // SE OBTIENEN LOS VALORES DE LOS CAMPOS
+        final String email = tilNombre.getEditText().getText().toString();
+        final String pass = tilPass.getEditText().getText().toString();
+
+        // SE VALIDAN QUE LOS CAMPOS NO ESTEN VACÍOS
+        if(email.isEmpty() || pass.isEmpty()){
+            Toast.makeText(this, "LLENA TODOS LOS CAMPOS", Toast.LENGTH_LONG).show();
+        }
+        else{
+            // SE OBTIENEN LOS RESULTADOS DE LA VALIDACIÓN DE AMBOS CAMPOS
+            Boolean vemail = validaemail(email);
+            Boolean vpass = validapassword(pass);
+
+            // SE VALIDA QUE LOS CAMPOS HAYAN PASADO SUS RESPECTIVAS VALIDACIONES
+            if(vemail && vpass) {
+                // AQUÍ SE REALIZA EL PROCESO PARA REALIZAR LA INSERCIÓN EN LA BASE DE DATOS
+                // MOSTRAMOS EL PROGRESS DIALOG ---- AQUÍ SE COMIENZA EL ARMADO Y LA EJECUCIÓN DEL WEB SERVICE
+                progressDialog.setMessage("CARGANDO...");
+                progressDialog.show();
+                // CREACIÓN DE LA CADENA A EJECUTAR EN EL WEB SERVICE MEDIANTE VOLLEY
+                // Objeto de volley
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpUri,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String serverresponse) {
+                                // UNA VEZ QUE SE MANDAN TODOS LOS VALORES AL WEB SERVICE
+                                // QUITAMOS EL PROGRESS DIALOG PARA QUE UNA VEZ QUE SE MANDEN LOS DATOS
+                                // YA SE PUEDA TRABAJAR
+                                progressDialog.dismiss();
+                                // MANEJO DE ERRORES CON RESPECTO A LA RESPUESTA
+                                try {
+                                    // CREAR UN OBJETO DE TIPO JSON PARA OBTENER EL ARCHIVO QUE MANDARÁ EL WEB SERVICE
+                                    JSONObject obj = new JSONObject(serverresponse);
+                                    // INTERPRETAR EL VALOR DEL JSON OBTENIDO DEL WEB SERVICE
+                                    Boolean status = obj.getBoolean("status");
+                                    // OBTENER EL MENSAJE
+                                    String mensaje = obj.getString("mensaje");
+                                    // INTERPRETAR LOS VALORES
+                                    if(status = false){
+                                        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
+                                        // REDIRIGE AL INICIO DE LA APP
+                                        Intent intent_inicio = new Intent(getApplicationContext(), inicioActivity.class);
+                                        startActivity(intent_inicio);
+                                    }
+                                }catch(JSONException e){
+                                    e.printStackTrace();
+                                }
+
+                            }
+                            // ESTE SE EJECUTA SI HAY UN ERROR EN LA RESPUESTA
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // SE OCULTA EL PROGRESS DIALOG
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+                    // MAPEO DE LOS VALORES QUE MANDAMOS AL WEB SERVICE
+                    protected Map<String, String> getParams(){
+                        // RETORNAR LOS VALORES
+                        Map<String, String> parametros = new HashMap<>();
+                        parametros.put("Correo", email);
+                        parametros.put("Contrasena", pass);
+                        return parametros;
+                    }
+                };
+
+                // SE MANDA A EJECUTAR EL STRING PARA LA LIBRERÍA DE VOLLEY
+                requestQueue.add(stringRequest);
+            }
+        }
     }
 
     // MÉTODO DE LA ACCIÓN DEL OYENTE PARA LA RECUPERACIÓN DE LA CONTRASEÑA
@@ -114,5 +262,31 @@ public class MainActivity extends AppCompatActivity {
             // SE MANDA A EJECUTAR EL ACTIVITY DE INICIO DE LA APP
             startActivity(intent1);
         }
+    }
+
+    private boolean validaemail(String noe){
+        // O SE ASEGURA QUE TENGA DATOS ESPECÍFICOS DE UN EMAIL
+        Pattern patronEmail = Patterns.EMAIL_ADDRESS;
+
+        // Y NO PASE DEL LÍMTE PERMITIDO
+        if(!patronEmail.matcher(noe).matches() || noe.length() > 50 ) {
+            tilNombre.setError("Correo no válido, Intentalo de nuevo");
+            return false;
+        }else{
+            tilNombre.setError(null);
+        }
+
+        return true;
+    }
+
+    private boolean validapassword(String password){
+        // VALIDA QUE NO PASE DEL LÍMTE PERMITIDO
+        if (password.length() > 16) {
+            tilPass.setError("Contraseña no válida, Intentalo de nuevo");
+            return false;
+        }else{
+            tilPass.setError(null);
+        }
+        return true;
     }
 }
